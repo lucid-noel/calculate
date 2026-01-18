4d840d0e887e49c0248e0fe431cf35fc56d0fa85
lucid-noel <lucid.noelx@gmail.com>
2026-01-18 23:03:47 +0900

    Add files via upload
    ** plus

--- calculate/c.py
+++ calculate/c.py
@@ -1,18 +1,20 @@
 a = float(input("number1: "))
 b = float(input("number2: "))
-op = input("choose (+,-,*,/): ")
+op = input("choose (+,-,*,/,**): ")
 
 if op == "+":
     result = a + b
 elif op == "-":
     result = a - b
+elif op == "**":
+    result = a ** b    
 elif op == "*":
     result = a * b
 elif op == "/":
     if b == 0:
         print("0 is not divisible")
         exit()
-        result = a/b
+        result = a / b        
 else:
     print("wrong")
     exit()
